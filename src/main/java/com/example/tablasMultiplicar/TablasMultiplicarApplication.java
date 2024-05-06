7f4777524d9cface16e291e994c765b3ce2b4dad
Victor Luevano <victorluevmon@gmail.com>
2024-05-06 12:34:45 -0600

    Se inició el proyecto

---
+++ src/main/java/com/example/tablasMultiplicar/TablasMultiplicarApplication.java
@@ -0,0 +1,13 @@
+package com.example.tablasMultiplicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablasMultiplicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablasMultiplicarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tablasMultiplicar/TablasMultiplicarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tablasMultiplicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablasMultiplicarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
